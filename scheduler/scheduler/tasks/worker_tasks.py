8fc51e0c6be1c1fe7b49a268b3ac826204dc50c3
barshakc <kcbarsha78@gmail.com>
2026-01-13 15:38:56 +0545

    Update worker_tasks

--- scheduler/scheduler/tasks/worker_tasks.py
+++ scheduler/scheduler/tasks/worker_tasks.py
@@ -1,10 +1,45 @@
-from scheduler.scheduler.celery_app import celery_app
+from scheduler.celery_app import celery_app
+from scheduler.db.database import SessionLocal
+from scheduler.models.task_model import Task
+from scheduler.models.task_run_model import TaskRun
+from scheduler.models.enums import TaskStatus
+from sqlalchemy.sql import func
 import time
 
 @celery_app.task(name="scheduler.tasks.worker_tasks.run_task")
-def run_task(task_id: int, task_name: str):
-    print(f"Starting task {task_name} (ID: {task_id})")
-    time.sleep(5)
-    print(f"Finished task {task_name} (ID: {task_id})")
-    return f"Task {task_id} done"
+def run_task(task_id: int, task_name: str, payload: dict = None):
+    db = SessionLocal()
+    try:
+        task_run = TaskRun(
+            task_id=task_id,
+            status=TaskStatus.active,
+        )
+        db.add(task_run)
+        db.commit()
+        db.refresh(task_run)
+
+        print(f"Starting task '{task_name}' (Task ID: {task_id}, Run ID: {task_run.id})")
+
+        if payload:
+            print(f"Payload: {payload}")
+        time.sleep(5)  
+        result = {"message": f"Task {task_id} completed successfully."}
+
+        task_run.status = TaskStatus.active 
+        task_run.finished_at = func.now()
+        task_run.result = result
+        db.commit()
+        db.refresh(task_run)
+
+        print(f"Finished task '{task_name}' (Task ID: {task_id}, Run ID: {task_run.id})")
+        return result
+
+    except Exception as e:
+        db.rollback()
+        task_run.status = TaskStatus.paused 
+        db.commit()
+        raise e
+    finally:
+        db.close()
+
 
